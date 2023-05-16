c2965aeee26b59a3a7859b3fda8a8f67473815a5
nce27 <nc3elliott@gmail.com>
2023-05-16 14:16:40 +0100

    Completed debugging challenge for section 05.

--- lib/challenge1.rb
+++ lib/challenge1.rb
@@ -1,10 +1,15 @@
 def get_most_common_letter(text)
   counter = Hash.new(0)
+  #p counter
   text.chars.each do |char|
-    counter[char] += 1
+      counter[char] += 1 unless char == " " 
   end
-  counter.to_a.sort_by { |k, v| v }[0][0]
+  p counter
+  counter.to_a.sort_by { |key, value| value }[-1][0]
 end
+
+p get_most_common_letter("the roof, the roof, the roof is on fire!")
+#p "o"
 
 # Intended output:
 # 
